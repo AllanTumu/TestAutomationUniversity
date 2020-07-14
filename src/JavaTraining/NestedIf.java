018a5e11f75eb9f8c76769a59d8fe7606ca4bfd9
AllanTumu <allantumu1962@gmail.com>
2020-07-14 16:11:12 +0300

    Changed the variable names and made the logic dynamic

--- src/JavaTraining/NestedIf.java
+++ src/JavaTraining/NestedIf.java
@@ -6,27 +6,28 @@ public class NestedIf {
     public static void main(String[] args) {
 
         //Initialize what we know
-        int salary = 30000;
-        int year = 2;
+        int requiredSalary = 30000;
+        int requiredYears = 2;
 
         //Get what we do not know
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your salary");
-        salary = scanner.nextInt();
+        double salary = scanner.nextDouble();
 
         System.out.println("Enter the number of years you have worked");
-        year = scanner.nextInt();
+        double years = scanner.nextDouble();
 
         //Make a decision if some one Qualifies for a Loan or not
-        if (salary>=30000) {
-            if (year>=2) {
+        if (salary>=requiredSalary) {
+            if (years>=requiredYears) {
                 System.out.println("You Qualify for a loan");
             }
-            else if (year<2) {
+            else if (years<requiredYears) {
                 System.out.println("You do not Qualify for a loan");
             }
         } else {
             System.out.println("You do not Qualify for the Loan scheme");
         }
+        scanner.close();
     }
 }
